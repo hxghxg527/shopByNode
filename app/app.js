a39200e57e240fc2e34f104040b702ef9880fb2a
hxghxg527 <hxghxg527@163.com>
2016-08-31 00:10:53 +0800

    add session

--- app/app.js
+++ app/app.js
@@ -4,11 +4,11 @@ var express = require('express'),
     app = express(),
     ejs = require('ejs'),
     path = require('path'),
+    session = require('express-session'),
     bodyParser = require('body-parser'),
     multer = require('multer');
 
 require('./db/dbConnection')();
-
 global.dbHelper = require('./db/dbHelper');
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -21,6 +21,28 @@ app.set('views', path.join(__dirname, 'views'));
 
 // use static files.
 app.use(express.static(path.join(__dirname, '../public')));
+
+// only use this middleware, in request param, it will has session key.
+app.use(session({
+    resave: true, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: 'secret',
+    cookie: {
+        maxAge: 1000 * 60 * 30
+    }
+}));
+
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user;
+    var error = req.session.error;
+    res.locals.message = '';
+
+    if (error) {
+        res.locals.message = "<div class='error-message'>" + error + "</div>";
+    }
+
+    next();
+});
 
 require('./routes')(app);
 
--- app/routes/register.js
+++ app/routes/register.js
@@ -10,7 +10,11 @@ module.exports = function (app) {
             password = req.body.password;
 
         Users.findOne({name: username}, function (err, user) {
-            if (user) {
+            if (err) {
+                req.session.error = "network exception.";
+                res.sendStatus(500);
+            } else if (user) {
+                req.session.error = "user is exist.";
                 res.sendStatus(403);
             } else {
                 Users.create({
@@ -18,8 +22,10 @@ module.exports = function (app) {
                     password: password
                 }, function (err) {
                     if (err) {
+                        req.session.error = "user is created failed.";
                         res.sendStatus(500);
                     } else {
+                        req.session.error = "user is created success.";
                         res.status(200).send('success!!');
                     }
                 });
